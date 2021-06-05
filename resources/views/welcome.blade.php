fda6024e0a554d99e8cd90139f9975872c909278
Roberto Arruda <roberto0arruda@hotmail.com>
2021-06-05 18:00:18 -0400

    feat: install tailwind css
    Signed-off-by: Roberto Arruda <roberto0arruda@hotmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,9 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
